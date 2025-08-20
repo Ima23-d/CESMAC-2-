869f15704ee0342a178255dd9e4a97b943358dc0
Ima23-d <153031237+Ima23-d@users.noreply.github.com>
2025-08-20 13:30:51 -0300

    Update Atv_3.py

--- CESMAC 2° periodo/Progamação 2 python/Atividades python/Lista_2/Desafio/Atv_3.py
+++ CESMAC 2° periodo/Progamação 2 python/Atividades python/Lista_2/Desafio/Atv_3.py
@@ -1,1 +1,21 @@
+while True:
+    n = int(input("Quantos números (entre 1 e 1000)? "))
+    if 1 <= n <= 1000:
+        break
+    else:
+        print("Erro! Digite uma quantidade entre 1 e 1000.")
+
+numeros = []
+for i in range(n):
+    while True:
+        num = float(input(f"Número {i+1}: "))
+        if 0 <= num <= 1000:
+            numeros.append(num)
+            break
+        else:
+            print("Erro! Digite um número entre 0 e 1000.")
+
+print(f"Menor: {min(numeros)}")
+print(f"Maior: {max(numeros)}")
+print(f"Soma: {sum(numeros)}")
 

6fe0a3349c155d51de022326cc9d802956610f85
Ima23-d <153031237+Ima23-d@users.noreply.github.com>
2025-08-20 13:26:34 -0300

    Update Atv_3.py

--- CESMAC 2° periodo/Progamação 2 python/Atividades python/Lista_2/Desafio/Atv_3.py
+++ CESMAC 2° periodo/Progamação 2 python/Atividades python/Lista_2/Desafio/Atv_3.py
@@ -1,10 +1,1 @@
-n = int(input("Quantos números? "))
 
-numeros = []
-for i in range(n):
-    num = float(input(f"Número {i+1}: "))
-    numeros.append(num)
-
-print(f"Menor: {min(numeros)}")
-print(f"Maior: {max(numeros)}")
-print(f"Soma: {sum(numeros)}")

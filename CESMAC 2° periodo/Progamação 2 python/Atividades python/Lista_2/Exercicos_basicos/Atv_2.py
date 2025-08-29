9f3971011d1ad92af7d401eeff91d861b518c164
Ima23-d <153031237+Ima23-d@users.noreply.github.com>
2025-08-29 17:18:27 -0300

    Update Atv_2.py

--- CESMAC 2° periodo/Progamação 2 python/Atividades python/Lista_2/Exercicos_basicos/Atv_2.py
+++ CESMAC 2° periodo/Progamação 2 python/Atividades python/Lista_2/Exercicos_basicos/Atv_2.py
@@ -1,8 +1,5 @@
 n = int(input("Digite o limite: "))
 num = 0
-while num <= 100:
+while num <= n:
     print(num)
     num += 1
-    if num == n:
-        print(f"Chegou ao número {n}, parando a contagem.")
-        break
